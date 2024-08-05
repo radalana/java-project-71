ba7cbd6f3437965a88766aed4ffd136fb7bba980
Svetlana <radalana415@gmail.com>
2024-08-05 16:31:00 +0200

    Chore build.gradle.kts * add picocli dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
 }
 
 tasks.test {
