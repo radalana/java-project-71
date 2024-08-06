bab82fb7ec33ec8e582fd96bd9feeb1132fa001d
Svetlana <radalana415@gmail.com>
2024-08-06 18:05:49 +0200

    update: App.java
    add call method implementation

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,10 +4,10 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import java.lang.Runnable;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0", description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
+public class App implements Callable<Integer> {
     @Option(names = { "-f", "--format" }, description = "Output format", defaultValue = "stylish")
     private String format;
     @Parameters(index = "0", description = "past to first file")
@@ -16,7 +16,11 @@ public class App implements Runnable {
     @Parameters(index = "1", description = "past to second file")
     private String filepath2;
 
-    @Override public void run() {}
+    @Override
+    public Integer call() throws Exception {
+        Differ.generate(filepath1, filepath2);
+        return 0;
+    }
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
