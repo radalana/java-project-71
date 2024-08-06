f7b3ab2aabbe5864e538326ae78faceb6780ddf1
Svetlana <radalana415@gmail.com>
2024-08-06 17:20:22 +0200

    chore: bump jackson-databind for json parsing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
 }
 
 tasks.test {
