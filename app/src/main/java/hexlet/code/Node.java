1b3d1780eb823320c2ce9e8c0632aa4334955c95
Svetlana <radalana415@gmail.com>
2024-08-21 17:43:16 +0200

    add Node class

---
+++ app/src/main/java/hexlet/code/Node.java
@@ -0,0 +1,36 @@
+package hexlet.code;
+
+import java.util.List;
+
+public class Node {
+    public enum Differ {
+        ADDED,
+        DELETED,
+        UNCHANGED,
+        CHANGED,
+        NESTED
+    }
+    public String key;
+    public Object value;
+    public Object newValue;
+    public Differ differ; //возможно нужен enum
+    public List<Object> children;
+    Node(String key, Object value, Differ differ) {
+        this.key = key;
+        this.value = value;
+        this.differ = differ;
+    }
+    Node(String key, Object oldValue, Object newValue, Differ differ) {
+        this.key = key;
+        this.value = oldValue;
+        this.newValue = newValue;
+        this.differ = differ;
+    }
+    @Override
+    public String toString() {
+        if (this.differ.equals(Differ.CHANGED)) {
+            return String.format("key = %s, old value = %s, new value = %s, %s\n", key, value, newValue, differ);
+        }
+        return String.format("key = %s, value = %s, %s\n", key, value, differ);
+    }
+}
