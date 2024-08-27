2fd02457e2454b23a92cc5437a5c0ef31d1ad454
Svetlana <radalana415@gmail.com>
2024-08-27 19:41:35 +0200

    style: DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,7 +2,8 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class DifferTest {
     @Test
@@ -11,14 +12,14 @@ class DifferTest {
         //ожидаемы результат в файле сравнить содержимое файлов
         String path1 = "files/file1.json";
         String path2 = "files/file2.json";
-        String expected = "{\n" +
-                "  - follow: false\n" +
-                "    host: hexlet.io\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
-                "  + timeout: 20\n" +
-                "  + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
 
         try {
             String actual = Differ.generate(path1, path2);
@@ -26,6 +27,6 @@ class DifferTest {
         } catch (Exception e) {
             e.printStackTrace();
             fail("Test failed due to an exception: " + e.getMessage());
-        };
+        }
     }
 }
