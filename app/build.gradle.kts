e82b121123b2a801ee6d296df2b5a57084f6b98d
Svetlana <radalana415@gmail.com>
2024-08-27 18:48:54 +0200

    chore: update build.gradle.kts main.yml Makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     application
     checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
@@ -22,6 +23,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
 
 application {
     mainClass.set("hexlet.code.App")
