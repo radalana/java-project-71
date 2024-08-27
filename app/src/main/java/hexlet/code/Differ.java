5af33c8abb1d9ce634a8c6b87382cc6638d3f1f8
Svetlana <radalana415@gmail.com>
2024-08-27 17:23:08 +0200

    fix: Differ.generate intends

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -50,16 +50,16 @@ public class Differ {
         var lines = tree.stream()
                 .map(node  -> {
                     String line = switch(node.differ) {
-                       case DELETED -> String.format("- %s: %s", node.key, node.value);
-                       case ADDED -> String.format("+ %s: %s", node.key, node.value);
-                       case CHANGED -> String.format("- %s: %s\n%s+ %s: %s", node.key, node.value, intend, node.key, node.newValue);
-                       case UNCHANGED -> String.format("  %s: %s", node.key, node.value);
+                       case DELETED -> intend + String.format("- %s: %s", node.key, node.value);
+                       case ADDED -> intend +String.format("+ %s: %s", node.key, node.value);
+                       case CHANGED -> intend + String.format("- %s: %s\n%s+ %s: %s", node.key, node.value, intend + intend, node.key, node.newValue);
+                       case UNCHANGED -> intend + String.format("  %s: %s", node.key, node.value);
                        default -> "Invalid";
                    };
                    return intend + line;
                 }).toList();
         String result = "{\n" + String.join("\n", lines) + "\n}";
         System.out.println(result);
-        return "";
+        return result;
     }
 }
