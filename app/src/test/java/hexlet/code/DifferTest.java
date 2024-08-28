3a1da3f546e2377147ca714950cb97449cfe2057
Svetlana <radalana415@gmail.com>
2024-08-28 18:56:28 +0200

    refactor: DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,20 +8,26 @@ import static org.junit.jupiter.api.Assertions.fail;
 class DifferTest {
     @Test
     void generate() throws Exception {
-        //путь файла = дать сразу
-        //ожидаемы результат в файле сравнить содержимое файлов
-        String path1 = getClass().getClassLoader().getResource("files/file1.json").getPath();
-        String path2 = getClass().getClassLoader().getResource("files/file2.json").getPath();
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
-
         try {
+            URL resource1 = getClass().getClassLoader().getResource("files/file1.json");
+            URL resource2 = getClass().getClassLoader().getResource("files/file2.json");
+
+            if (resource1 == null || resource2 == null) {
+                fail("Test failed because the resource files could not be found.");
+            }
+
+            String path1 = resource1.getPath();
+            String path2 = resource2.getPath();
+
+            String expected = "{\n"
+                    + "  - follow: false\n"
+                    + "    host: hexlet.io\n"
+                    + "  - proxy: 123.234.53.22\n"
+                    + "  - timeout: 50\n"
+                    + "  + timeout: 20\n"
+                    + "  + verbose: true\n"
+                    + "}";
+
             String actual = Differ.generate(path1, path2);
             assertEquals(expected, actual);
         } catch (Exception e) {
